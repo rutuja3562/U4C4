133ec727b1ec21f33332589cd9ae04cec2a8dc87
rutuja3562 <rspatil3562@gmail.com>
2022-02-21 17:02:57 +0530

    push

--- Template-U3C4/components/sidebar.js
+++ Template-U3C4/components/sidebar.js
@@ -1,20 +1,4 @@
-// function sidebar(){
-//     return
-//     ` <h1>Masai News</h1>
-//     <p>Log In</p>
-//     <input type="text">
-//     <p>Startups</p>
-//     <p>Newsletters</p>
-//     <p>Audio</p>
-//     <p>Vedio</p>
-// `
 
-//     // return your html component here
-//     // Make sure to give input search box id as ""
-
-  
-// }
-// export default sidebar;
 function sidebar(){
     return `
     <h1>Masai News</h1>
